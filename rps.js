61edaebca923c0c4bb0d91e511bf4af12aee7d9a
JerryShum <jerryshum2636@gmail.com>
2024-01-03 13:30:55 -0500

    feat: created simple html page and rps logic is working

--- rps.js
+++ rps.js
@@ -18,6 +18,41 @@
 
 */
 
+//! Selecting elements
+const buttons = document.querySelectorAll('button')
+console.log(buttons);
+
+
+buttons.forEach(buttons => {
+    console.log(buttons.value);
+    buttons.addEventListener('click', playRound);
+});
+
+//! Logic:
+
+function playRound() {
+    let playerSelection = this.value.toLowerCase();
+    let computerSelection = getComputerChoice();
+    console.log(playerSelection);
+
+    if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        console.log("You win! Rock beats scissors");
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        console.log("You win! Paper beats rock");
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        console.log("You win! Scissors beats paper");
+    }
+    else if (playerSelection == computerSelection) {
+        console.log("It's a tie!");
+    }
+    else {
+        console.log("You lose! " + computerSelection + " beats " + playerSelection);
+    }
+}
+
+
 function getComputerChoice() {
     // Gets a random number between 0 - 2
     let num = Math.floor(Math.random() * 3);
@@ -33,37 +68,7 @@ function getComputerChoice() {
 
 }
 
-function getPlayerChoice() {
-    let input;
 
-    do {
 
-        input = prompt("Please enter your choice, Rock, Paper, or Scissors").toLowerCase();
-    } while (input != "rock" || input != "paper" || input != "scissors")
 
-    return input;
-
-}
-
-let playerSelection = getPlayerChoice();
-let computerSelection = getComputerChoice();
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return "You win! Rock beats scissors";
-    }
-    else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return "You win! Paper beats rock";
-    }
-    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return "You win! Scissors beats paper";
-    }
-    else if (playerSelection == computerSelection) {
-        return "It's a tie!";
-    }
-    else {
-        return "You lose! " + computerSelection + " beats " + playerSelection;
-    }
-}
-
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
